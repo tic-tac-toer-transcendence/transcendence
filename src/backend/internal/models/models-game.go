0b78330ce29f604b448b33d9a3fda0aaa2ef0c56
atdeters <sreted@outlook.com>
2026-09-24 00:40:44 +0200

    feat: add first prototype of data structures

---
+++ src/backend/internal/models/models-game.go
@@ -0,0 +1,34 @@
+package models-game
+
+type Owner int
+
+const (
+    None Owner = iota
+    Cross
+    Circle
+)
+
+func (o Owner) String() string {
+    switch o {
+    case Cross:
+        return "X"
+    case Circle:
+        return "O"
+    default:
+        return "None"
+    }
+}
+
+type FieldIn struct {
+	top    [3]int
+	mid    [3]int
+	bottom [3]int
+	Owner Owner
+}
+
+type FieldOut struct {
+	top    [3]FieldIn
+	mid    [3]FieldIn
+	bottom [3]FieldIn
+	Owner Owner
+}
