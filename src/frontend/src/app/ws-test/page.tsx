b90627ec341f1a827a8624fec51953f012597bcb
Serhii Veretennikov <gridis72@gmail.com>
2026-09-25 09:12:04 +0200

    feat: implemented sidebar

---
+++ src/frontend/src/app/ws-test/page.tsx
@@ -0,0 +1,10 @@
+import Sidebar from "@/components/sidebar"
+
+export default function WsTest() {
+	return (
+		<main className="flex">
+			<Sidebar></Sidebar>
+			Under construction
+		</main>
+	)
+}
--- src/frontend/src/components/sidebar.tsx
+++ src/frontend/src/components/sidebar.tsx
@@ -1,7 +1,17 @@
+import Link from "next/link";
+
 export default function Sidebar() {
 	return (
-		<aside className="absolute top-0 left-0 h-screen w-[250]">
-			Sidebar
+		<aside className="h-screen w-[250] bg-section-bg border-r border-section-border">
+			<nav className="p-[24] flex flex-col">
+				<section className="flex flex-col gap-[16]">
+					<button><Link href="/" className="text-white text-sm uppercase font-black">Tic Tac Toer</Link></button>
+					<ul className="flex flex-col gap-[8]">
+						<li><Link href="/play" className="bg-button-bg border border-button-border px-[12] py-[14] block rounded-xl">Play</Link></li>
+						<li><Link href="/ws-test" className="bg-button-bg border border-button-border px-[12] py-[14] block rounded-xl">Ws Test</Link></li>
+					</ul>
+				</section>
+			</nav>
 		</aside>
 	)
 }
